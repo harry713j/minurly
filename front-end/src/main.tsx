b8292d93a68d28a98406fa9ebfa4a63929f8f5f4
Harry <hariharasethi89@gmail.com>
2025-08-11 17:31:36 +0530

    Set path alias and router

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -1,13 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./global.css";
-import App from "./App.tsx";
 import { HeroUIProvider } from "@heroui/react";
+import { Login } from "@/pages";
+import { RouterProvider, createBrowserRouter } from "react-router";
+
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <HeroUIProvider>
-      <App />
+      <RouterProvider router={router} />
     </HeroUIProvider>
   </StrictMode>
 );
