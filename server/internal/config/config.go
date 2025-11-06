ae435f644e0843f1889d51e4dc9907aa759c2c18
Harry <hariharasethi89@gmail.com>
2025-11-06 22:05:52 +0530

    change config

--- server/internal/config/config.go
+++ server/internal/config/config.go
@@ -3,22 +3,56 @@ package config
 import (
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/gorilla/sessions"
-	"github.com/joho/godotenv"
+	_ "github.com/joho/godotenv/autoload"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 )
 
-var (
+type Config struct {
+	Primary  *PrimaryConfig
+	Server   *ServerConfig
+	Database *DatabaseConfig
+	Auth     *AuthConfig
+}
+
+type PrimaryConfig struct {
+	Env string
+}
+
+type ServerConfig struct {
+	Port               string
+	ReadTimeOut        int
+	WriteTimeOut       int
+	IdleTimeOut        int
+	CORSAllowedOrigins []string
+}
+
+type AuthConfig struct {
 	OAuthConfig  *oauth2.Config
-	SessionStore = sessions.NewCookieStore([]byte(os.Getenv("SESSION_KEY")))
-)
+	SessionStore *sessions.CookieStore
+}
 
-func init() {
-	godotenv.Load()
+type DatabaseConfig struct {
+	DbURL string
+}
 
-	OAuthConfig = &oauth2.Config{
+func LoadConfig() *Config {
+	primary := &PrimaryConfig{Env: "development"}
+
+	port := os.Getenv("PORT")
+	whiteLists := strings.Split(os.Getenv("ALLOWED_ORIGIN"), ",")
+	server := &ServerConfig{
+		Port:               port,
+		ReadTimeOut:        10,
+		WriteTimeOut:       15,
+		IdleTimeOut:        60,
+		CORSAllowedOrigins: whiteLists,
+	}
+
+	oAuthConfig := &oauth2.Config{
 		ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
 		ClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
 		Scopes:       []string{"email", "profile"},
@@ -26,11 +60,31 @@ func init() {
 		Endpoint:     google.Endpoint,
 	}
 
-	SessionStore.Options = &sessions.Options{
+	sessionStore := sessions.NewCookieStore([]byte(os.Getenv("SESSION_KEY")))
+
+	auth := &AuthConfig{
+		OAuthConfig:  oAuthConfig,
+		SessionStore: sessionStore,
+	}
+
+	auth.SessionStore.Options = &sessions.Options{
 		Path:     "/",
 		MaxAge:   86400 * 7,
 		HttpOnly: true,
 		Secure:   false,                // for dev
 		SameSite: http.SameSiteLaxMode, // for dev only
 	}
+
+	db := &DatabaseConfig{
+		DbURL: os.Getenv("DATABASE_URL"),
+	}
+
+	mainConfig := &Config{
+		Primary:  primary,
+		Server:   server,
+		Auth:     auth,
+		Database: db,
+	}
+
+	return mainConfig
 }
